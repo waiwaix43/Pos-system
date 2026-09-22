66d0627f414ddd3893ab18945055ffe7ede10c84
Natthaset2547 <73818820+waiwaix43@users.noreply.github.com>
2026-09-22 10:30:24 +0700

    Update page.tsx

--- frontend/app/pos/edit/[id]/page.tsx
+++ frontend/app/pos/edit/[id]/page.tsx
@@ -147,7 +147,7 @@ export default function EditOrderPage() {
         const savedUser = JSON.parse(localStorage.getItem("userContext") || "null");
         localStorage.removeItem("userContext");
         router.push(savedUser?.pin_enabled === false ? '/' : '/pin');
-      }} className="py-6 px-6 text-left text-gray-300 border-t border-[#666666] hover:bg-[#666666] transition-colors text-[16px]">{JSON.parse(localStorage.getItem("userContext") || "null")?.pin_enabled === false ? 'ออกจากระบบ' : 'กลับสู่หน้า PIN'}</button>
+      }} className="py-6 px-6 text-left text-gray-300 border-t border-[#666666] hover:bg-[#666666] transition-colors text-[16px]">{typeof window !== "undefined" ? (JSON.parse(localStorage.getItem("userContext") || "null")?.pin_enabled === false ? 'ออกจากระบบ' : 'กลับสู่หน้า PIN') : 'กลับสู่หน้า PIN'}</button>
     </div>
 
       {/* POS EDIT VIEW */}
